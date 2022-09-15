70645d3ee017a1c662fcaca2c2ebf9a69f6aa79b
jacob Calderon <Jacob.a.calderon@gmail.com>
2022-09-15 10:40:06 -0500

    update rest blog

---
+++ src/test/java/com/example/jacobrestblog/JacobRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacobrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
