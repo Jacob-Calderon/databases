d43587e8a117e481e43917c0daa4451435bc42b3
jacob Calderon <Jacob.a.calderon@gmail.com>
2022-09-20 15:26:00 -0500

    updating

--- src/main/java/controller/AnimalController.java
+++ src/main/java/controller/AnimalController.java
@@ -1,13 +1,44 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public class AnimalController {
 
-public AnimalController(){
+    private Object fetchAnimalFacts;
+
+    public AnimalController(){
 
 }
 
 @GetMapping({"/animal"})
+    public String fetchAnimalById(){
 
+    return "Capybara!!!";
 }
+    @GetMapping({"/animal/{id}"})
+
+    public String fetchAnimalFacts(@PathVariable long id)
+    {return fetchAnimalFacts.fakeAnimalFacts(id);}
+
+        switch((int)id)
+
+    static {
+
+        case 1:
+            String s = "Find the animals!!";
+
+        case 2:
+            String a = "mvjhgvjhgvjhgvjhgvhj";
+
+
+        default:
+            String s1 = "unknown animal";
+
+
+
+    }
+
+
+    }
+
